05d60e3cde58c34056f3cabfacb04b2e0ddb07d5
fnpdoke <63687515+fnpdoke@users.noreply.github.com>
2022-07-15 00:35:25 +0800

    Create ToBottom.user.js

---
+++ js/JuqTokPRi0CLBisDPn4WQg/userscript/ToBottom.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         ToBottom
+// @namespace    fpdk
+// @version      0.1
+// @description  ToBottom
+// @author       fpdk
+// @match        https://www.ptt.cc/bbs/Lifeismoney/M.1657470157.A.733.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.scrollTo(0, document.body.scrollHeight);
+})();
