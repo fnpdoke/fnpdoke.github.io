765cab3631ee301259a8edbc7fef0c08ab030826
fnpdoke <63687515+fnpdoke@users.noreply.github.com>
2022-09-04 00:07:51 +0800

    Update ToBottom.user.js

--- js/JuqTokPRi0CLBisDPn4WQg/userscript/ToBottom.user.js
+++ js/JuqTokPRi0CLBisDPn4WQg/userscript/ToBottom.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ToBottom
 // @namespace    fpdk
-// @version      20220806
+// @version      20220904
 // @description  ToBottom
 // @author       fpdk
 // @match        https://www.ptt.cc/bbs/Lifeismoney/M.1662179399.A.251.html
