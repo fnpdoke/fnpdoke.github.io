f263b96ca82e72cfb91cccebb87f6a4a310d1948
fnpdoke <63687515+fnpdoke@users.noreply.github.com>
2022-11-10 23:14:11 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+let delay = () => {
+    return new Promise((resolve, reject) => { setTimeout(resolve, 1500); });
+};
+
+async function p() {
+    let win = window.open('', 'p', 'width=100, height=100');
+    let list = document.querySelectorAll('p a');
+    let len = list.length;
+    let cnt = 1;
+    for(let item of list) {
+        win.location.href = item.getAttribute("href");
+        await delay();
+        cnt++;
+        if (cnt > len) {
+            win.close();
+            b();
+        }
+    }
+}
+
+let b = () => {
+    document.querySelectorAll('b a').forEach(e => e.click())
+}
