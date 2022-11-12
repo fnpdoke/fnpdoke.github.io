bcaf2cd2e0d1a83a4e9b9a6f5ee84ecc23760809
fnpdoke <63687515+fnpdoke@users.noreply.github.com>
2022-11-12 12:08:11 +0800

    Update ToBottom.user.js

--- js/JuqTokPRi0CLBisDPn4WQg/userscript/ToBottom.user.js
+++ js/JuqTokPRi0CLBisDPn4WQg/userscript/ToBottom.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         ToBottom
 // @namespace    fpdk
-// @version      20221105
+// @version      20221112
 // @description  ToBottom
 // @author       fpdk
-// @match        https://www.ptt.cc/bbs/Lifeismoney/M.1667497798.A.088.html
+// @match        https://www.ptt.cc/bbs/Lifeismoney/M.1668191988.A.77D.html
 // @grant        none
 // ==/UserScript==
 
