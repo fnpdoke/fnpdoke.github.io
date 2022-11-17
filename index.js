83f507376c9f38274205d3793e432557122e24f9
fnpdoke <63687515+fnpdoke@users.noreply.github.com>
2022-11-18 01:55:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let delay = () => {
     return new Promise((resolve, reject) => { setTimeout(resolve, 1500); });
 };
 
-async function p() {
-    let win = window.open('', 'p', 'width=100, height=100');
+async function p(e) {
+    let win = window.open('', 'p', 'width=100,height=100,left=' + e?.screenX + ',top=' + e?.screenY);
     let list = document.querySelectorAll('p a');
     let len = list.length;
     let cnt = 1;
