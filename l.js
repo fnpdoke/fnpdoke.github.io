b1de1769fdb3e0c56344a188e7901bd96e3818ca
fnpdoke <63687515+fnpdoke@users.noreply.github.com>
2023-05-27 22:57:44 +0800

    Update l.js

--- l.js
+++ l.js
@@ -3,7 +3,7 @@ let delay = () => {
 };
 
 async function p(e) {
-    let win = window.open('', 'p', 'width=100,height=100,left=' + e?.screenX + ',top=' + e?.screenY);
+    let win = window.open('', 'p', 'noreferrer,width=100,height=100,left=' + e?.screenX + ',top=' + e?.screenY);
     let list = document.querySelectorAll('p a');
     let len = list.length;
     let cnt = 1;
