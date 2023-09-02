805476c2a24bdc08e49dd0ac7f4803456cf6b805
fnpdoke <63687515+fnpdoke@users.noreply.github.com>
2023-09-03 00:35:13 +0800

    Update l.js

--- l.js
+++ l.js
@@ -1,19 +1,5 @@
-let delay = () => {
-    return new Promise((resolve, reject) => { setTimeout(resolve, 2500); });
-};
-
-async function p(e) {
-    let win = window.open('', 'p', 'width=100,height=100,left=' + e?.screenX + ',top=' + e?.screenY);
-    let list = document.querySelectorAll('p a');
-    let len = list.length;
-    let cnt = 1;
-    for(let item of list) {
-        if (win.closed) break;
-        win.location.href = item.getAttribute("href");
-        await delay();
-        cnt++;
-        if (cnt > len) win.close();
-    }
+let p = () => {
+    document.querySelectorAll('p a').forEach(e => e.click())
 }
 
 let b = () => {
