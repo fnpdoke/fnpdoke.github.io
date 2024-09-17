990e55f5c514e49ec050dedce9d2c6419e2bf192
fnpdoke <63687515+fnpdoke@users.noreply.github.com>
2024-09-17 15:19:00 +0800

    Update l.js

--- l.js
+++ l.js
@@ -1,14 +1,15 @@
 let delay = () => {
     return new Promise((resolve, reject) => { setTimeout(resolve, 2000); });
 }
-
-let p = () => {
-    document.querySelectorAll('p a').forEach(e => e.click())
+let p = (e) => {
+    go(e, document.querySelectorAll('p a'));
+    b(e);
 }
-
-async function b(e) {
-    let win = window.open('', 'b', 'width=800,height=600,left=' + e?.screenX + ',top=' + e?.screenY);
-    let list = document.querySelectorAll('b a');
+let b = (e) => {
+    go(e, document.querySelectorAll('b a'));
+}
+async function go(e, list) {
+    let win = window.open('', 'go', 'width=800,height=600,left=' + e?.screenX + ',top=' + e?.screenY);
     let len = list.length;
     let cnt = 1;
     for(let item of list) {
