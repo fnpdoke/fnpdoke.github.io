85b7bf80612a453365aec5de03efcac1e751c9bd
fnpdoke <63687515+fnpdoke@users.noreply.github.com>
2025-05-08 02:24:38 +0800

    分工

--- l.js
+++ l.js
@@ -1,11 +1,19 @@
-let p = (e) => {
-    go(e, document.querySelectorAll('p a'));
+let win = [];
+let p1 = (e) => {
+    go(e, document.querySelectorAll('.p1 p a'));
+}
+let p2 = (e) => {
+    go(e, document.querySelectorAll('.p2 p a'));
 }
 let b = (e) => {
     go(e, document.querySelectorAll('b a'));
 }
 function go(e, list) {
     for(let item of list) {
-        window.open(item.getAttribute('href'));
+        let a = window.open(item.getAttribute('href'));
+        win.push(a);
     }
+}
+function close() {
+    win.forEach(w => w.close());
 }
